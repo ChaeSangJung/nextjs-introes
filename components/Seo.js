9468297683da1f374334bdbc9b3ee0ed5596cfb6
Chae Sang, Jung <shtingstar@hanmail.net>
2022-01-18 23:50:35 +0900

    2.8 404 Pages

--- components/Seo.js
+++ components/Seo.js
@@ -3,8 +3,8 @@ import Head from "next/head";
 export default function Seo({title, description}){
     return(
         <Head>
+            <meta description={description ? description : "nextjs" } />
             <title>{title} | Next Movies</title>
-            <meta description={description} />
         </Head>
     );
 }
